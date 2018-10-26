ca18a70601f0782f21eb09aa747d5ad7f742493c
Anuken <arnukren@gmail.com>
2018-10-26 10:29:00 -0400

    AI block cache

--- core/src/io/anuke/mindustry/ai/control/AI.java
+++ core/src/io/anuke/mindustry/ai/control/AI.java
@@ -1,13 +1,18 @@
 package io.anuke.mindustry.ai.control;
 
+import com.badlogic.gdx.utils.ObjectMap;
+import com.badlogic.gdx.utils.ObjectSet;
 import io.anuke.mindustry.ai.control.tasks.BuildBlockTask;
 import io.anuke.mindustry.content.Items;
 import io.anuke.mindustry.content.blocks.ProductionBlocks;
 import io.anuke.mindustry.entities.traits.BuilderTrait.BuildRequest;
 import io.anuke.mindustry.entities.units.types.WorkerDrone;
+import io.anuke.mindustry.game.EventType.TileChangeEvent;
 import io.anuke.mindustry.game.Team;
 import io.anuke.mindustry.type.Recipe;
+import io.anuke.mindustry.world.Block;
 import io.anuke.mindustry.world.Tile;
+import io.anuke.ucore.core.Events;
 import io.anuke.ucore.entities.EntityGroup;
 import io.anuke.ucore.entities.impl.BaseEntity;
 
@@ -15,9 +20,23 @@ import static io.anuke.mindustry.Vars.*;
 
 public class AI{
     private final Team team;
+    private ObjectMap<Block, ObjectSet<Tile>> blocks = new ObjectMap<>();
+
+    private Block drillBlock = ProductionBlocks.mechanicalDrill;
 
     public AI(Team team) {
         this.team = team;
+
+        Events.on(TileChangeEvent.class, event -> {
+            for(ObjectSet<Tile> set : blocks.values()){
+                set.remove(event.tile);
+            }
+
+            if(event.tile.getTeam() == team){
+                if(!blocks.containsKey(event.tile.block())) blocks.put(event.tile.block(), new ObjectSet<>());
+                blocks.get(event.tile.block()).add(event.tile);
+            }
+        });
     }
 
     public void update(){
@@ -32,14 +51,23 @@ public class AI{
         }
     }
 
+    ObjectSet<Tile> getBlock(Block block){
+        if(!blocks.containsKey(block)) blocks.put(block, new ObjectSet<>());
+        return blocks.get(block);
+    }
+
     void assignTask(WorkerDrone drone){
+        if(getBlock(drillBlock).size > 10){
+            //pathfind to core.
+        }
+
         drone.beginTask(new BuildBlockTask(getClosestDrillReq()));
     }
 
     BuildRequest getClosestDrillReq(){
         Tile core = state.teams.get(team).cores.first();
         Tile tile = world.indexer().findClosestOre(core.drawx(), core.drawy(), Items.copper);
-        return new BuildRequest(tile.x, tile.y, 0, Recipe.getByResult(ProductionBlocks.mechanicalDrill));
+        return new BuildRequest(tile.x, tile.y, 0, Recipe.getByResult(drillBlock));
     }
 
 }
